f838d53af490cbf1ccfc3d965ed26e2dea50ffb9
Edward-Edo <edwarditriagosub@gmail.com>
2019-10-04 16:06:24 -0400

    first commit

---
+++ formulario.php
@@ -0,0 +1,11 @@
+<?php
+
+$valor1=$_POST['valor'];
+$valor2=$_POST['valor2'];
+$valor3=$_POST['valor3'];
+echo $valor1;
+echo $valor2;
+echo $valor3;
+
+
+?>
---
+++ hp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>prueba</title>
+    <script type="text/javascript">
+    function porcentaje(porcentaje){
+     var porcentaje= porcentaje;
+document.getElementById('hiden').value = porcentaje;
+    }
+  </script>
+</head>
+<body>
+ <div id="td" class="td" value="escriba un nombre"> </div>
+    <input type="radio" name="1" value="3" onchange="porcentaje(this.td)">
+<input type="text" id="hiden" name="" value="">
+</body>
+</html>
+ </script>
